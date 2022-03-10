aec67257fbf474f6143bf4a1db2e596c80bd2f4d
Akseli Hirvelä <akseli.hirvela@gmail.com>
2022-03-10 09:19:52 +0200

    rest

--- src/main/java/com/example/akselibookstore/web/BookController.java
+++ src/main/java/com/example/akselibookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package com.example.akselibookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.akselibookstore.domain.Book;
 import com.example.akselibookstore.domain.BookRepository;
@@ -29,6 +33,18 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+    
+    // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get a book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }  
      
     @RequestMapping(value = "/add")
     public String addBook(Model model){
