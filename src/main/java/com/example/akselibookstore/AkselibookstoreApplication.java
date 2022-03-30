7fe56f0ff62c71974680271c3688bb4560ce7e9f
Akseli Hirvelä <akseli.hirvela@gmail.com>
2022-03-30 19:32:52 +0300

    Spring security: user entity, UserDetailService, added users to database (user & admin), configuration changes

--- src/main/java/com/example/akselibookstore/AkselibookstoreApplication.java
+++ src/main/java/com/example/akselibookstore/AkselibookstoreApplication.java
@@ -12,6 +12,9 @@ import com.example.akselibookstore.domain.BookRepository;
 import com.example.akselibookstore.domain.Category;
 import com.example.akselibookstore.domain.CategoryRepository;
 
+import com.example.akselibookstore.domain.User;
+import com.example.akselibookstore.domain.UserRepository;
+
 @SpringBootApplication
 public class AkselibookstoreApplication {
 	
@@ -22,7 +25,7 @@ public class AkselibookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("### CREATING DEFAULT CATEGORIES");
 			crepository.save(new Category("Fantasy"));
@@ -54,6 +57,17 @@ public class AkselibookstoreApplication {
 				bcount++;
 			}
 			log.info("### " + bcount + " BOOKS AVAILABLE");
+			
+			// Create users: admin/admin user/user
+			log.info("### CREATING USERS: USER & ADMIN");
+			urepository.save(new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER"));
+			urepository.save(new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN"));
+			int ucount = 0;
+			for (User user: urepository.findAll()) {
+				log.info(user.toString());
+				ucount++;
+			}
+			log.info("### " + ucount + " USERS EXISTING");
 		};
 	}
 }
